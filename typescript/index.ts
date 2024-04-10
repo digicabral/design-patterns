0a3b9941d57b8c8006d6ab71f7ec9a94b846bdb6
Rodrigo Siqueira Cabral <rodrigo.cabral@luizalabs.com>
2024-04-10 09:24:43 -0300

    fix: right implementation

--- typescript/index.ts
+++ typescript/index.ts
@@ -1,14 +1,32 @@
 //High level modules should not depend on low level modules. Both should depend on abstractions
 //Abstractions should not depende on details. Details should depend on abstractions
+interface IDatabase {
+  save(data: string): void;
+}
+class MySqlDatabase implements IDatabase {
+  save(data: string): void {
+    console.log(`${data} is being saved my SQL`);
+  }
+}
 
-//wrong implementation
-class MySqlDatabase {
-  save(data: string): void {}
+class MongoDBDatabase implements IDatabase {
+  save(data: string): void {
+    console.log(`${data} is being saved mongo`);
+  }
 }
 
 class HighLevelModule {
-  constructor(private database: MySqlDatabase) {}
-  execute(data: string) {
+  constructor(private database: IDatabase) {}
+  execute(data: string): void {
     this.database.save(data);
   }
 }
+
+const mysql = new MySqlDatabase();
+const mongo = new MongoDBDatabase();
+
+let user = new HighLevelModule(mysql);
+user.execute("Man");
+
+let post = new HighLevelModule(mongo);
+user.execute("New Post");
