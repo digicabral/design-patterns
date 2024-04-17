86a8d95ed56c870f69f3abc32eff0d586bb8bf8d
Rodrigo Siqueira Cabral <rodrigo.cabral@luizalabs.com>
2024-04-17 16:37:42 -0300

    feat: add builder basic example

---
+++ typescript/index.ts
@@ -0,0 +1,77 @@
+interface Builder {
+  setPartA(): void;
+  setPartB(): void;
+  setPartC(): void;
+}
+
+class Product {
+  private parts: string[] = [];
+
+  public add(part: string): void {
+    this.parts.push(part);
+  }
+
+  public listParts(): void {
+    console.log(`Product parts: ${this.parts.join(", ")}`);
+  }
+}
+
+class ConcreteBuilder implements Builder {
+  private product!: Product;
+
+  constructor() {
+    this.reset();
+  }
+
+  public reset(): void {
+    this.product = new Product();
+  }
+
+  public setPartA(): void {
+    this.product.add("PartA");
+  }
+
+  public setPartB(): void {
+    this.product.add("PartB");
+  }
+
+  public setPartC(): void {
+    this.product.add("PartC");
+  }
+
+  public getProduct(): Product {
+    const result = this.product;
+    this.reset();
+    return result;
+  }
+}
+
+class Director {
+  private builder!: Builder;
+
+  public setBuilder(builder: Builder): void {
+    this.builder = builder;
+  }
+
+  public buildMinimumProduct(): void {
+    this.builder.setPartA();
+  }
+
+  public buildFullProduct(): void {
+    this.builder.setPartA();
+    this.builder.setPartB();
+    this.builder.setPartC();
+  }
+}
+
+const builder = new ConcreteBuilder();
+const director = new Director();
+director.setBuilder(builder);
+
+director.buildMinimumProduct();
+let minProduct = builder.getProduct();
+console.log(minProduct);
+
+director.buildFullProduct();
+let fullProduct = builder.getProduct();
+console.log(fullProduct);
